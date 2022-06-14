6418abaf8ea901ad8b4dca030b9e56db92c909a8
michael-fares-lrn <michael.fares@learnosity.com>
2022-06-14 09:55:42 -0500

    project init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const App = () =>{
+    return (
+        <div className="main-container">
+            <h1 className="welcome glow">
+                Hey? How's it going?
+            </h1>
+        </div>
+    )
+}
+
+export default App;
